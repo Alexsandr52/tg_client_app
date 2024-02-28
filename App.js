5966b78fe3db68ec03e9b81f2b2b4312009c30b3
Alexsander52 <ak.polyanskiy@google.com>
2024-02-28 21:26:00 +0300

    add some

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-//  consst keys = require('./keys.json');
+// const keys = require('./keys.json');
 const tg = window.Telegram.WebApp;
 let user_input = {};
 let temp_choice = {};
@@ -10,9 +10,9 @@ const onClose = () => {
   console.log(fetchDataFromURL());
 };
 
-tg.MainButton.onClick(send);
-tg.MainButton.text = "Оплатить";
-
+// tg.MainButton.onClick(check_list);
+tg.MainButton.onClick();
+tg.MainButton.text = "Продолжить";
 tg.SettingsButton.show();
 // tg.showAlert(tg.initDataUnsafe.user);
 
@@ -93,6 +93,7 @@ function main_button_checkout(temp_choice) {
 }
 
 function main_view_function(dates) {
+  
   const valid_days = Object.keys(dates).map((dateString) => {
     const day = dateString.split(".")[0];
     return day;
@@ -100,6 +101,18 @@ function main_view_function(dates) {
   const calendar_view = document.querySelector(".calendar_view");
   const time_view = document.querySelector(".time_view");
   const cinema_view = document.querySelector(".cinema_view");
+  const price_list = document.querySelector(".prise-list");
+
+  //price
+  function make_price_list() {
+    cinema_view.style.display = "None";
+    calendar_view.style.display = "None";
+    time_view.style.display = "None";
+    price_list.style.display = "block";
+
+    price_list.querySelector('.time_header_1').innerHTML = `${tg.initDataUnsafe.user.first_name}`
+    
+  }
 
   // Обработчики для места
   function click_handler_second(e) {
@@ -139,6 +152,7 @@ function main_view_function(dates) {
 
   // Сетка мест
   function cinima_grid(palaces, date, time) {
+    price_list.style.display = "None";
     calendar_view.style.display = "None";
     time_view.style.display = "None";
     cinema_view.style.display = "block";
@@ -190,6 +204,7 @@ function main_view_function(dates) {
       }
     });
 
+    price_list.style.display = "None";
     cinema_view.style.display = "None";
     calendar_view.style.display = "None";
     time_view.style.display = "block";
@@ -220,6 +235,7 @@ function main_view_function(dates) {
     temp_choice = {};
     tg.BackButton.hide();
 
+    price_list.style.display = "None";
     cinema_view.style.display = "None";
     time_view.style.display = "None";
     calendar_view.style.display = "block";
@@ -245,5 +261,145 @@ function main_view_function(dates) {
   }
 
   make_calendar();
-  console.log("loaded");
 }
+
+// let dates = {"27.02.2024":{"11:00":[["1","1","Свободно"],["1","2","Свободно"],["1","3","Свободно"],["2","1","Забронировано"],["2","2","Забронировано"],["2","3","Свободно"],["3","1","Забронировано"],["3","2","Свободно"],["3","3","Свободно"]],"15:00":[["1","1","Забронировано"],["1","2","Свободно"],["1","3","Свободно"],["1","4","Забронировано"],["2","1","Свободно"],["2","2","Забронировано"],["2","3","Свободно"],["2","4","Забронировано"],["3","1","Свободно"],["3","2","Свободно"],["3","3","Забронировано"],["3","4","Забронировано"],["4","1","Свободно"],["4","2","Забронировано"],["4","3","Забронировано"],["4","4","Забронировано"],["4","5","Забронировано"],["4","6","Свободно"]]},"28.02.2024":{"17:00":[["1","1","Забронировано"],["1","2","Свободно"],["1","3","Свободно"],["1","4","Свободно"],["1","5","Забронировано"],["1","6","Забронировано"],["2","1","Забронировано"],["2","2","Забронировано"],["2","3","Забронировано"]],"23:00":[["2","4","Свободно"],["2","5","Забронировано"],["2","6","Забронировано"],["3","1","Забронировано"],["3","2","Забронировано"],["3","3","Забронировано"],["3","4","Забронировано"],["3","5","Свободно"],["3","3","Забронировано"]]},"29.02.2024":{"23:50":[["1","1","Забронировано"],["1","2","Свободно"],["1","3","Забронировано"],["1","4","Забронировано"],["1","5","Свободно"],["1","6","Свободно"],["2","1","Свободно"],["2","2","Забронировано"],["2","3","Свободно"],["2","4","Свободно"],["2","5","Забронировано"],["2","6","Свободно"],["3","1","Свободно"],["3","2","Свободно"],["3","3","Забронировано"],["3","4","Забронировано"],["3","5","Свободно"],["3","6","Забронировано"]]}}
+// const valid_days = Object.keys(dates).map((dateString) => {
+//   const day = dateString.split(".")[0];
+//   return day;
+// });
+
+
+// // Обработчики для места
+// function click_handler_second(e) {
+//   if (!Object.keys(temp_choice).includes(e.target.parentNode.id)) {temp_choice[e.target.parentNode.id] = [];}
+//   temp_choice[e.target.parentNode.id].push(e.target.id);
+
+//   if (temp_choice[e.target.parentNode.id].indexOf(e.target.id) !== -1) {
+//     temp_choice[e.target.parentNode.id] = temp_choice[e.target.parentNode.id].filter(function (item) {return item !== e.target.id;});
+//     if (temp_choice[e.target.parentNode.id].length == 0) {delete temp_choice[e.target.parentNode.id];}
+//   }
+
+//   main_button_checkout(temp_choice);
+//   e.target.removeEventListener("click", click_handler_second);
+//   e.target.classList.remove("selected");
+//   e.target.addEventListener("click", click_handler);
+// }
+
+// function click_handler(e) {
+//   if (!Object.keys(temp_choice).includes(e.target.parentNode.id)) {temp_choice[e.target.parentNode.id] = [];}
+//   temp_choice[e.target.parentNode.id].push(e.target.id);
+
+//   main_button_checkout(temp_choice);
+//   e.target.removeEventListener("click", click_handler);
+//   e.target.addEventListener("click", click_handler_second);
+//   e.target.classList.add("selected");
+// }
+
+
+// // Показ карточек
+// {
+//   const calendar_view = document.querySelector('.calendar_view');
+//   const time_view = document.querySelector('.time_view');
+//   const cinema_view = document.querySelector('.cinema_view');
+//   const price_list = document.querySelector('.prise-list');
+  
+//   // Показ элементов 
+//   function change_view_mode(box_name){
+//     price_list.style.display = "None";
+//     cinema_view.style.display = "None";
+//     time_view.style.display = "None";
+//     calendar_view.style.display = "None";
+
+//     switch (box_name){
+//       case 'price_list': price_list.style.display = "block"; break;
+//       case 'cinema_view': cinema_view.style.display = "block"; break;
+//       case 'time_view': time_view.style.display = "block"; break;
+//       case 'calendar_view': calendar_view.style.display = "block"; break;
+//     }
+//   }
+
+//   // Календарь
+//   function make_calendar() {
+//     temp_choice = {};
+//     tg.BackButton.hide();
+
+//     change_view_mode('calendar_view');
+//     const calendar = document.querySelector('.calendar');
+//     calendar.innerHTML = `<p class="weekday">Пн</p> <p class="weekday">Вт</p> <p class="weekday">Ср</p> <p class="weekday">Чт</p> <p class="weekday">Пт</p> <p class="weekday">Сб</p> <p class="weekday">Вс</p> `;
+
+//     for (let i = 1; i <= 30; i++) {
+//       if (valid_days.includes(i.toString())) {calendar.innerHTML += `<div id="${i}" class="active">${i}</div>`;
+//       }else {calendar.innerHTML += `<div id="${i}" class="disabled-celendar">${i}</div>`;
+//       }
+//     }
+
+//     // Обработчикик на кнопки дней
+//     for (let el in valid_days) {
+//       document.getElementById(valid_days[el]).addEventListener('click', (e) => {
+//         user_input['date_choice'] = Object.keys(dates)[valid_days.indexOf(e.target.id)];
+//         set_time(user_input['date_choice']);
+//       });
+//     }
+//   }
+
+//   // Время
+//   function set_time(date) {
+//     tg.BackButton.show();
+//     tg.BackButton.onClick(function () {make_calendar()});
+
+//     change_view_mode('time_view');
+
+//     document.querySelector('.time_header_1').innerHTML = date;
+//     document.querySelector('.avalibal_time').innerHTML = '';
+
+//     const time_data = dates[date];
+//     const time = Object.keys(time_data);
+
+//     for (let i in time) {document.querySelector('.avalibal_time').innerHTML += `<div class="time_item" id="${100 + i}">${time[i]}</div>`;}
+
+//     for (let i in time) {
+//       document.getElementById(100 + i).addEventListener("click", (e) => {
+//         user_input['time_choice'] = time[i];
+//         cinima_grid(dates[date][time[i]], date, time[i]);
+//       });
+//     }
+//   }
+
+//   // Сетка мест
+//   function cinima_grid(palaces, date, time) {
+//     change_view_mode('cinema_view');
+
+//     let cinema_cheme = {};
+//     let cinema_place = document.querySelector(".cinema_place");
+//     document.querySelector(".time_header").innerHTML = `${date} ${time}`;
+
+//     for (let i in palaces) {
+//       if (Object.keys(cinema_cheme).includes(palaces[i][0])) {cinema_cheme[palaces[i][0]][palaces[i][1]] = palaces[i][2];
+//       } else {
+//         cinema_cheme[palaces[i][0]] = {};
+//         cinema_cheme[palaces[i][0]][palaces[i][1]] = palaces[i][2];
+//       }
+//     }
+
+//     cinema_place.innerHTML = '<div class="display"></div>';
+
+//     for (let i in cinema_cheme) {
+//       let plases_string = "";
+
+//       for (let id in cinema_cheme[i]) {
+//         let status = cinema_cheme[i][id];
+//         if (status == "Свободно") {plases_string += `<div id="${id}" class="active active_place">${id}</div>`;
+//         } else {plases_string += `<div id="${id}" class="disabled-celendar">${id}</div>`;}
+//       }
+
+//       let new_row = `<div id="row_${i}" class="row">${plases_string} </div>`;
+//       cinema_place.innerHTML += new_row;
+//     }
+
+//     let elements = document.querySelectorAll(".active_place");
+//     for (let i = 0; i < elements.length; i++) {elements[i].addEventListener("click", click_handler);}
+//   }
+
+//   make_calendar();
+// }
