632cc8046ed6d20c77aad76f6f03623b778e9bfd
Alexsander52 <ak.polyanskiy@google.com>
2024-03-04 14:50:40 +0300

    we back

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function save_data() {
     body: JSON.stringify(user_input),
   })
     .then((response) => response.json())
-    .then((data) => tg.showAlert(data))
+    .then((data) => tg.showAlert(JSON.parse(data)))
     .catch((error) => {
       tg.showAlert("Ошибка сохранения данных: " + error);
     });
